52988aeb09087ae4753c36b05dfd8c29c9b273ed
Supratim Nag <supratimnag1997@gmail.com>
2021-08-21 11:36:19 +0530

    added : sidebar footer

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 import './sidebar.css';
 import logo from '../../assets/images/logo.png';
 import sidebar_items from '../../assets/JsonData/sidebar_routes.json';
-import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
 
 const SidebarItems = (props) => {
   const active = props.active ? 'active' : '';
@@ -16,25 +19,83 @@ const SidebarItems = (props) => {
   );
 };
 
+const externalPaths = [
+  {
+    id: 'linkedin',
+    path: 'https://www.linkedin.com/in/nagsupratim/',
+    class: 'bx bxl-linkedin-square',
+    tip: 'Linkedin 😁',
+  },
+  {
+    id: 'github',
+    path: 'https://github.com/nagSupratim/crm-admin',
+    class: 'bx bxl-github',
+    tip: 'Source Code 🧐',
+  },
+  {
+    id: 'email',
+    path: 'mailto:supratimnag1997@gmail.com',
+    class: 'bx bx-mail-send',
+    tip: 'Email me 🤭',
+  },
+];
+
 const Sidebar = (props) => {
+  const themeReducer = useSelector((state) => state.ThemeReducer);
   const activeItem = sidebar_items.findIndex(
     (item) => item.route === props.location.pathname
   );
+  const tooltipConfig = {
+    type: themeReducer.mode === 'theme-mode-light' ? 'dark' : 'light',
+    delayShow: '100',
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar__logo">
         <img src={logo} alt="company logo" />
       </div>
-      <div className="">
-        {sidebar_items.map((item, index) => (
-          <Link to={item.route} key={index}>
-            <SidebarItems
-              title={item.display_name}
-              icon={item.icon}
-              active={index === activeItem}
-            />
-          </Link>
-        ))}
+      {sidebar_items.map((item, index) => (
+        <Link to={item.route} key={index}>
+          <SidebarItems
+            title={item.display_name}
+            icon={item.icon}
+            active={index === activeItem}
+          />
+        </Link>
+      ))}
+      <div className="sidebar__footer">
+        <div className="sidebar__footer-row">
+          <div className="row">
+            {externalPaths.map((item) => (
+              <div className="col-4" key={item.id}>
+                <Link
+                  to={{ pathname: item.path }}
+                  target="_blank"
+                  data-tip={item.tip}
+                >
+                  <i className={item.class}></i>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="sidebar__footer-row">
+          <div className="row">
+            <Link
+              to={{ pathname: 'https://github.com/nagSupratim' }}
+              target="_blank"
+              data-tip="To my Github"
+            >
+              <div className="col-12">
+                <span>&copy; nagSupratim</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <ReactTooltip {...tooltipConfig} />
       </div>
     </div>
   );
